b351d273b9307e8cc3e425cc5dc8554c5c649e7f
ravi <ravi@vytec.com>
2023-12-17 16:26:47 +0530

    updated

--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,22 @@
+resource "tls_private_key" "oskey" {
+  algorithm = "RSA"
+}
+ 
+resource "local_file" "myterrakey" {
+  content  = tls_private_key.oskey.private_key_pem
+  filename = "user34v1.pem"
+}
+ 
+resource "aws_key_pair" "key121" {
+  key_name   = "user34v1"
+  public_key = tls_private_key.oskey.public_key_openssh
+}
+
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0759f51a90924c166"
   instance_type = "t2.micro"
-  key_name = "user34"
+  key_name      = aws_key_pair.key121.key_name
   tags = {
     Name = "User34_test"
   }
