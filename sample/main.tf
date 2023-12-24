6439dbb238291ffe50fb31c70c18fb007e39628d
ravi <ravi@vytec.com>
2023-12-24 16:52:33 +0530

    added

--- sample/main.tf
+++ sample/main.tf
@@ -9,7 +9,14 @@ resource "aws_s3_bucket" "example" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
-
+  key_name = "user34"
+  security_groups = ["launch-wizard-15"]
+  user_data = <<EOF
+   #!/bin/bash
+   sudo apt update
+   sudo apt install nginx -y
+   sudo systemctl enable nginx
+  EOF
   tags = {
     Name = "ExampleAppServerInstance"
   }
