17ecdac7ad819647af24b113ad709bb9a80e9f61
ravi <ravi@vytec.com>
2023-12-26 14:56:55 +0530

    added

--- network/network.tf
+++ network/network.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "11.0.1.0/24"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "Main"
   }
