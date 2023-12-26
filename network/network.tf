87d7acb83ff349d7f780aeeb60e13c18f8c618cd
ravi <ravi@vytec.com>
2023-12-26 15:07:12 +0530

    added rt

--- network/network.tf
+++ network/network.tf
@@ -12,6 +12,32 @@ resource "aws_subnet" "main" {
   cidr_block = "11.0.1.0/24"
   map_public_ip_on_launch = true
   tags = {
-    Name = "Main"
+    Name = "main"
   }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
