69c8061102e1a4a811ba3a4dc791a6d78dae3700
ravi <ravi@vytec.com>
2023-12-28 16:01:14 +0530

    aded

--- network/main.tf
+++ network/main.tf
@@ -1,4 +1,12 @@
-module "abcelb"{
+module "vpc"{
+  source = "./vpc"
+  tags = var.tags
+}
+module "instances"{
+  source = "./ec2"
+  tags = var.tags
+}
+module "elb"{
   source = "./elb"
   tags = var.tags
 }
