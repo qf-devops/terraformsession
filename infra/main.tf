0c8c5248a393dac4c5afeb71bf6fa6f056d87cc7
ravi <ravi@vytec.com>
2023-12-29 14:25:35 +0530

    adeed

--- infra/main.tf
+++ infra/main.tf
@@ -2,11 +2,11 @@ module "vpc"{
   source = "./vpc"
   tags = var.tags
 }
-module "instances"{
-  source = "./ec2"
-  tags = var.tags
-}
-module "elb"{
-  source = "./elb"
-  tags = var.tags
-}
+# module "instances"{
+#   source = "./ec2"
+#   tags = var.tags
+# }
+# module "elb"{
+#   source = "./elb"
+#   tags = var.tags
+# }
