99aeaea3b51f0b89eba44c30621a25dede18e739
ravi <ravi@vytec.com>
2023-12-29 14:57:57 +0530

    added

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ module "instances"{
   source = "./ec2"
   tags = var.tags
   instance_type = "t2.micro"
-  vpc_id = module.vpc.vpcids
+  vpc_id = module.vpc.vpcid
   subnet_id1 = module.vpc.subnetid1
   subnet_id2 = module.vpc.subnetid2
 }
