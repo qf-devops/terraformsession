644a195bff7ca9e35669302c2c5a75bee5360d0d
ravi <ravi@vytec.com>
2023-12-30 14:34:18 +0530

    added

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,9 @@
 module "vpc"{
-  source     = "./vpc"
+  source     = "git@github.com:terraform30122023/vpc.git"
   tags       = var.tags
 }
 module "instances"{
-  source         = "./ec2"
+  source         = "git@github.com:terraform30122023/ec2.git"
   tags           = var.tags
   instance_type  = "t2.micro"
   vpc_id         = module.vpc.vpcid
@@ -11,7 +11,7 @@ module "instances"{
   subnet_id2     = module.vpc.subnetid2
 }
 module "elb"{
-  source        = "./elb"
+  source        = "git@github.com:terraform30122023/elb.git"
   tags          = var.tags
   subnet_id1    = module.vpc.subnetid1
   subnet_id2    = module.vpc.subnetid2
