cf12d2945f42cfec3e316b848f8e7c16fd4f2fe8
ravi <ravi@vytec.com>
2024-01-02 15:18:33 +0530

    added

--- lambda/main.tf
+++ lambda/main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "my_lambda" {
   timeout          = 10
   memory_size      = 128
 
-  source_code_hash = filebase64("lambda_function.zip")
+  filename         = "lambda_function.zip"
 
   role = aws_iam_role.lambda_execution_role.arn
 
