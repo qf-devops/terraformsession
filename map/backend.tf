a4997414a32f788f125f65f81de2beb4ae3cee60
ravi <ravi@vytec.com>
2024-01-04 15:00:10 +0530

    added

--- map/backend.tf
+++ map/backend.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "pg" {
-    conn_str = "postgres://postgres:postgres@db/terraform_backend"
+    conn_str = "postgres://postgres:postgres@db/terraform_backend?sslmode=disable"
   }
 }
