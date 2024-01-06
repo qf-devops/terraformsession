31a793d15cad262a2e54a4aade545b192eaee6d3
ravi <ravi@vytec.com>
2024-01-06 16:04:42 +0530

    added

--- docker/main.tf
+++ docker/main.tf
@@ -1,7 +1,10 @@
 resource "docker_container" "n1" {
   name  = "n1"
   image = docker_image.nginx.image_id
-  ports = [80]
+  ports {
+    internal = "80"
+    external = "80"
+  }
 }
 
 resource "docker_image" "nginx" {
