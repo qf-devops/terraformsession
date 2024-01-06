3a5468f7fe7497f4ba1716fbd63e39520dee20ec
ravi <ravi@vytec.com>
2024-01-06 16:33:38 +0530

    added

--- docker/provider.tf
+++ docker/provider.tf
@@ -7,6 +7,12 @@ terraform {
   }
 }
 
+# provider "docker" {
+#   host = "unix:///var/run/docker.sock"
+# }
+
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  host     = "ssh://ubuntu@23.23.28.233:22"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+#   key_material  = file(pathexpand("~/.docker/key.pem"))
 }
