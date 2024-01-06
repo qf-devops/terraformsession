33ff111e61839c74983aac955081400da9b49959
ravi <ravi@vytec.com>
2024-01-06 17:26:19 +0530

    added

--- docker/provider.tf
+++ docker/provider.tf
@@ -6,11 +6,12 @@ terraform {
     }
   }
 }
-
+#local dopcker host
 # provider "docker" {
 #   host = "unix:///var/run/docker.sock"
 # }
 
+# remote docker host
 provider "docker" {
   host = "tcp://23.23.28.233:2375"
 }
