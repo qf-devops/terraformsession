105853ac789aa6fe6b33dadae9b070538b1b376c
ravi <ravi@vytec.com>
2024-01-07 15:45:37 +0530

    added

--- multiprovider/main.tf
+++ multiprovider/main.tf
@@ -48,7 +48,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "demo-eks-cluster"]
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     command     = "aws"
   }
 }
