ea6bf6de9c3e41660ac83a091244a695c16a265e
ravi <ravi@vytec.com>
2024-01-07 16:19:28 +0530

    added

--- multiprovider/main.tf
+++ multiprovider/main.tf
@@ -39,11 +39,11 @@ data "aws_eks_cluster_auth" "example" {
   name = "demo-eks-cluster"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.example.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.example.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.example.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.example.token
+# }
 
 # resource "kubernetes_namespace" "nginx" {
 #   provider = kubernetes.eks_cluster
