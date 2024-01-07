a6ecb9b25e5c44f4b9796e286adc558c7c0b2bd8
ravi <ravi@vytec.com>
2024-01-07 15:41:38 +0530

    added

--- multiprovider/main.tf
+++ multiprovider/main.tf
@@ -36,15 +36,16 @@ module "eks" {
 #   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 # }
 
-data "aws_eks_cluster" "example" {
-  name = "demo-eks-cluster"
-}
-data "aws_eks_cluster_auth" "example" {
-  name = "demo-eks-cluster"
-}
+# data "aws_eks_cluster" "example" {
+#   name = "demo-eks-cluster"
+# }
+# data "aws_eks_cluster_auth" "example" {
+#   name = "demo-eks-cluster"
+# }
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.example.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", "demo-eks-cluster"]
